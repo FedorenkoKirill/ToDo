1d6a31d102e5670e8606f0bcb0f283fa8224f901
FedorenkoKirill <fedorenkokiri@bk.ru>
2022-08-12 18:45:55 +0300

    finally

--- app/models/Task.scala
+++ app/models/Task.scala
@@ -2,42 +2,18 @@ package models
 
 import play.api.libs.json.{Format, Json}
 import reactivemongo.play.json._
-import reactivemongo.bson.BSONObjectID
-import reactivemongo.bson._
+import reactivemongo.api.bson.{BSONDocumentHandler, Macros}
 
 case class Task(
-               _id:Option[BSONObjectID],
                label: String,
                done: Boolean,
                deleted: Boolean
                )
 
 
-trait TaskJson {
-  implicit val writes : Format[Task] = Json.format[Task]
+trait TaskWrites {
+  implicit val writesJson : Format[Task] = Json.format[Task]
+  implicit val writesBson : BSONDocumentHandler[Task] = Macros.handler[Task]
 }
 
-object Task extends TaskJson {
-
-  implicit object TaskBSONReader extends BSONDocumentReader[Task] {
-    def read(doc: BSONDocument): Task = {
-      Task(
-        doc.getAs[BSONObjectID]("_id"),
-        doc.getAs[String]("label").get,
-        doc.getAs[Boolean]("done").get,
-        doc.getAs[Boolean]("deleted").get
-      )
-    }
-  }
-
-  implicit object TaskBSONWriter extends BSONDocumentWriter[Task] {
-    def write(task: Task): BSONDocument = {
-      BSONDocument(
-        "_id" -> task._id,
-        "label" -> task.label,
-        "done" -> task.done,
-        "deleted" -> task.deleted
-      )
-    }
-  }
-}
+object Task extends TaskWrites
